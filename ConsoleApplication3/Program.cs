b58b9c77ef60de2b7f8a491e6d82cbc9fe4d0973
AnnaNika <programmka77@mail.ru>
2016-04-03 21:12:55 +0300

    Создана рамочка

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -4,12 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication3
+namespace MySnake
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.SetBufferSize(80, 25);
+
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 0, 24, '+');
+            VerticalLine rightLine = new VerticalLine(78, 0, 24, '+');
+            upLine.Draw();
+            downLine.Draw();
+            leftLine.Draw();
+            rightLine.Draw();
+
+            Console.ReadKey();
+
+
         }
     }
 }
--- ConsoleApplication3/VerticalLine.cs
+++ ConsoleApplication3/VerticalLine.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace MySnake
 {
-    class VerticalLine
+    class VerticalLine: Figure
     {
-    }
+            public VerticalLine(int x, int yUp, int yDown, char sym)
+            {
+                pList = new List<Point>();
+                for (int y = yUp; y <= yDown; y++)
+                {
+                    Point p = new Point(x, y, sym);
+                    pList.Add(p);
+
+                }
+            }
+        }
 }
